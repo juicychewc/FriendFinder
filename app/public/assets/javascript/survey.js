837c9561a699c0b6cc3e466080b7a07e13efd2b9
Sushichan <collin.chew.7188@gmail.com>
2018-09-15 22:22:18 -0700

    setup for heroku

--- app/public/assets/javascript/survey.js
+++ app/public/assets/javascript/survey.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-
-    $(".submit").on("click", function (event) {
+    $("#submit").on("click", function (event) {
         event.preventDefault();
 
         var questionsAnswers = $("#surveyQuestions").serializeArray();
@@ -15,36 +14,14 @@ $(document).ready(function () {
             answers: answersArray
         }
 
-        var matchedFriendName;
-        var matchedFriendPhoto;
+        $.post('/api/friends', newFriend).then(function (data) {
 
-        app.post("/api/friends", function (req, res) {
-            var scoreDifference = 9001;
-            var user = req.body;
-            var inputAnswers = user.answers;
-            for (i = 0; i < friends.length; i++) {
-                var difference = 0;
-                for (let ii = 0; ii < inputAnswers.length; ii++) {
-                    difference += Math.abs(friends[i].answers[ii] - inputAnswers[ii])
-                }
-                if (difference < scoreDifference) {
-                    scoreDifference = difference;
-                    matchedFriendName = friends[i].friendName;
-                    matchedFriendPhoto = friends[i].photo;
-                    return matchedFriendName;
-                    return matchedFriendPhoto;
-
-                }
-            }
-        })
-
-        $("#matchedUserName").text(matchedFriendName);
-        $("#matchedUserImage").attr("src", matchedFriendPhoto);
-        $(".modal").modal({
-            dismissible: true,
-        });
-        $(".modal").modal("open");
-
-        friends.push(newFriend);
-    });
-})
+            $("#matchedUserName").text(data.matchedFriendName);
+            $("#matchedUserImage").attr("src", data.matchedFriendPhoto);
+            $(".modal").modal({
+                dismissible: true
+            });
+            $(".modal").modal("open");
+        }); // post
+    }); // submit
+}); // doc ready
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,26 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
+
+    app.post("/api/friends", function (req, res) {
+        var user = req.body;
+        var inputAnswers = user.answers;
+        
+        var scoreDifference = 9001;
+                
+        for (i = 0; i < friends.length; i++) {
+            var difference = 0;
+            for (let ii = 0; ii < inputAnswers.length; ii++) {
+                difference += Math.abs(friends[i].answers[ii] - inputAnswers[ii])
+            }
+            if (difference < scoreDifference) {
+                scoreDifference = difference;
+                matchedFriendName = friends[i].friendName;
+                matchedFriendPhoto = friends[i].photo;
+            }
+        }
+        friends.push(user);
+
+        res.json({matchedFriendName: matchedFriendName, matchedFriendPhoto: matchedFriendPhoto });
+    })
 }    
